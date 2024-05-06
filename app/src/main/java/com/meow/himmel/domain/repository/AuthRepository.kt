bbe8ed686fdbba8831fe82b7eb0fc70f911bfdd8
meowlet <ngoclink13@gmail.com>
2024-05-06 22:39:24 +0700

    Fix: Change some return types

--- app/src/main/java/com/meow/himmel/domain/repository/AuthRepository.kt
+++ app/src/main/java/com/meow/himmel/domain/repository/AuthRepository.kt
@@ -3,8 +3,8 @@ import com.meow.himmel.domain.model.Response
 import kotlinx.coroutines.flow.Flow
 
 interface AuthRepository {
-    fun isUserLoggedIn(): Boolean
-    fun signIn(email: String, password: String): Flow<Response<Boolean>>
-    fun signUp(email: String, password: String): Flow<Response<Boolean>>
-    fun signOut(): Flow<Response<Boolean>>
+    fun isUserSignedIn(): Boolean
+    fun signIn(email: String, password: String): Flow<Response<Unit>>
+    fun signUp(email: String, password: String): Flow<Response<Unit>>
+    fun signOut(): Flow<Response<Unit>>
 }
