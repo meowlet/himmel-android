f8b2e6f28a397530a7b926725e14752fbfdb3d6c
meowlet <ngoclink13@gmail.com>
2024-05-06 22:40:03 +0700

    Feature: Add more UseCase to the wrapper

--- app/src/main/java/com/meow/himmel/domain/use_case/UseCase.kt
+++ app/src/main/java/com/meow/himmel/domain/use_case/UseCase.kt
@@ -1,7 +1,13 @@
 package com.meow.himmel.domain.use_case
 
+import com.meow.himmel.domain.use_case.auth.SignIn
+import com.meow.himmel.domain.use_case.auth.UserCheck
+import com.meow.himmel.domain.use_case.auth.SignOut
 import com.meow.himmel.domain.use_case.auth.SignUp
 
 data class UseCase(
+    val userCheck: UserCheck,
     val signUp: SignUp,
+    val signIn: SignIn,
+    val signOut: SignOut
 )
