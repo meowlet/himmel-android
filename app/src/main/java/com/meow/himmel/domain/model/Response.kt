866976c6ce8874f24870952db1a0dc79ece23d55
meowlet <ngoclink13@gmail.com>
2024-05-06 22:38:51 +0700

    Feature: Make Response model

--- app/src/main/java/com/meow/himmel/domain/model/Response.kt
+++ app/src/main/java/com/meow/himmel/domain/model/Response.kt
@@ -3,5 +3,6 @@ package com.meow.himmel.domain.model
 sealed class Response<out T> {
     data class Success<T>(val data: T) : Response<T>()
     data class Failure(val exception: Exception) : Response<Nothing>()
-    data object Waiting : Response<Nothing>()
+    data object Loading : Response<Nothing>()
+    data object Idling : Response<Nothing>()
 }
